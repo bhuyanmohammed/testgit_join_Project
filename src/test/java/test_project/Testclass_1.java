663590dbc98d8380a23724a6138e5f48a0838415
biplob <bhuyan.shiful1979@gmail.com>
2020-12-26 19:55:04 -0500

    Second commit.....

---
+++ src/test/java/test_project/Testclass_1.java
@@ -0,0 +1,20 @@
+package test_project;
+
+public class Testclass_1 {
+
+	public static void main(String[] args) {
+
+
+		
+		String hungry="yes";
+		if(hungry =="yes") {
+			System.out.println("i am hungry");
+			
+		}
+		else {
+			System.out.println("i am not hungry");
+		}
+		
+	}
+
+}
